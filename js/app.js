460524ab5a1367d8b7f8521f11c47c6a263365ba
kumelkalen <free.your.mind.fc@gmail.com>
2017-06-05 00:50:01 -0400

    Agregamos clases y elementos que usaremos para cambiar el nav

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-app.js
+function cambioNav (){
+	var scroll = window.pageYOffset; // Retorna los píxeles desplazados verticalmente
+	var nav = document.getsElementsByClassName("cont-menu");
+	var contenedorLogo = document.getsElementsByClassName("cont-logo");
+	var contenedorMenu = document.getsElementsByClassName("menu");
+	var botonSignUp = document.getsElementsByClassName("boton-signup");
+	var letra = document.getsElementsByClassName("letra");
+
+	if(scroll != 0){
+		botonSignUp.classList.toggle("display-boton");
+	}
+}
+
+window.addEventListener('scroll', cambioNav);
