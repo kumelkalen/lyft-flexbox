232d8e93cebd70c90e2407777d0146e9d63a63a7
kumelkalen <free.your.mind.fc@gmail.com>
2017-06-06 13:06:11 -0400

    Creamos nuestro footer y las propiedades de sus elementos, ademas al video le quitamos propiedades que interferían con el posicionamiento del footer.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ window.addEventListener('scroll', function () {
 var scroll = document.documentElement.scrollTop || window.pageYOffset // Retorna los píxeles desplazados verticalmente
 var segundoMenu = document.getElementsByClassName("nav2");
 
-	if(scroll > 50){
-		segundoMenu.classList.toggle("display-boton");
+	if(scroll > 50){ 
+		segundoMenu.classList.remove("display-boton"); // Hacemos visible nuestro segundo menú
+	}else{
+		segundoMenu.classList.add("display-boton"); // Lo volvemos invisible
 	}
-});
+})();
